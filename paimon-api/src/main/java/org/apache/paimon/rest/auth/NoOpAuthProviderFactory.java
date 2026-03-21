89a8c58b138aed3779077b18907d8a45d9da51a4
dongtingting <dongtingting@kuaishou.com>
2026-03-21 20:48:00 +0800

    add comment

--- paimon-api/src/main/java/org/apache/paimon/rest/auth/NoOpAuthProviderFactory.java
+++ paimon-api/src/main/java/org/apache/paimon/rest/auth/NoOpAuthProviderFactory.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.paimon.rest.auth;
 
 import org.apache.paimon.options.Options;
@@ -5,20 +23,22 @@ import org.apache.paimon.options.Options;
 import java.util.HashMap;
 import java.util.Map;
 
-public class NoOpAuthProviderFactory implements AuthProviderFactory  {
+/** Factory for {@link NoOpAuthProviderFactory}. */
+public class NoOpAuthProviderFactory implements AuthProviderFactory {
 
-  @Override
-  public String identifier() {
-    return AuthProviderEnum.NOOP.identifier();
-  }
+    @Override
+    public String identifier() {
+        return AuthProviderEnum.NOOP.identifier();
+    }
 
-  @Override
-  public AuthProvider create(Options options) {
-    return new AuthProvider() {
-      @Override
-      public Map<String, String> mergeAuthHeader(Map<String, String> baseHeader, RESTAuthParameter restAuthParameter) {
-        return new HashMap<>();
-      }
-    };
-  }
+    @Override
+    public AuthProvider create(Options options) {
+        return new AuthProvider() {
+            @Override
+            public Map<String, String> mergeAuthHeader(
+                    Map<String, String> baseHeader, RESTAuthParameter restAuthParameter) {
+                return new HashMap<>();
+            }
+        };
+    }
 }
