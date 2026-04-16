22cb200e3ef77f712505e6cdc0d9d0fce0454261
liuchaoyang03 <liuchaoyang03@kuaishou.com>
2026-04-16 19:05:54 +0800

    build: bump to 1.4.903
    1. bump to 1.4.903

--- paimon-python/setup.py
+++ paimon-python/setup.py
@@ -17,7 +17,7 @@
 ##########################################################################
 from setuptools import find_packages, setup
 
-VERSION = "1.4.902"
+VERSION = "1.4.903"
 
 PACKAGES = find_packages(include=["pypaimon*"])
 
