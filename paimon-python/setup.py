7806c978e1551423551543b196d246bf55f85e5c
liuchaoyang03 <liuchaoyang03@kuaishou.com>
2026-04-20 17:13:13 +0800

    build: pypaimon bump to 1.4.905
    1. pypaimon bump to 1.4.905

--- paimon-python/setup.py
+++ paimon-python/setup.py
@@ -17,7 +17,7 @@
 ##########################################################################
 from setuptools import find_packages, setup
 
-VERSION = "1.4.904"
+VERSION = "1.4.905"
 
 PACKAGES = find_packages(include=["pypaimon*"])
 
